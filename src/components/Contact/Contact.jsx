7fa6cba2141d519b11d9ea6868110856363c3b65
chijioke <superkingsely@gmail.com>
2024-07-01 10:26:12 -0700

    okay final

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -48,7 +48,9 @@ const Contact = () => {
         <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="user_name" className="user"  placeholder="Name"/>
           <input type="email" name="user_email" className="user" placeholder="Email"/>
-          <textarea name="message" className="user" placeholder="Message"/>
+          <textarea 
+          style={{resize:'none'}}
+          name="message" className="user" placeholder="Message"/>
           <input type="submit" value="Send" className="button"/>
           <span>{done && "Thanks for Contacting me"}</span>
           <div
